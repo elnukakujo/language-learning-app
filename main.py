696acaa7a0e08572a87766f938eacccff0bed04f
elnukakujo <jagernoe1s3@hotmail.com>
2025-06-01 18:39:52 +0200

    Refactored the pipeline and the overall file positions, added the grammar sheets to the database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Text
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 import pandas as pd
+import json
+import os
 
 engine = create_engine('sqlite:///vocab.db')
 
@@ -12,8 +14,8 @@ class Unit(Base):
     __abstract__ = True
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False, unique=True)
-    description = Column(String, nullable=True)
-    objectives = Column(String, nullable=True)
+    description = Column(Text, nullable=True)
+    objectives = Column(Text, nullable=True)
     level = Column(String, nullable=False)
 
     def __repr__(self):
@@ -24,6 +26,8 @@ class ChineseUnit(Unit):
 
     # One unit has many ChineseVocabulary entries
     vocab = relationship("ChineseVocabulary", back_populates="unit", cascade="all, delete-orphan")
+    # One unit has many ChineseGrammarSheet entries
+    grammar_sheets = relationship("ChineseGrammarSheet", back_populates="unit", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"<ChineseUnit(title='{self.title}', level='{self.level}')>"
@@ -52,29 +56,68 @@ class ChineseVocabulary(Vocabulary):
     def __repr__(self):
         return f"<ChineseVocabulary(word='{self.word}', pinyin='{self.pinyin}', translation='{self.translation}', type='{self.type}', level='{self.level}')>"
 
+class GrammarSheet(Base):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False, unique=True)
+    content = Column(Text, nullable=False)
+    level = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<GrammarSheet(title='{self.title}', level='{self.level}')>"
+    
+class ChineseGrammarSheet(GrammarSheet):
+    __tablename__ = 'chinese_grammar_sheets'
+
+    unit_id = Column(Integer, ForeignKey('chinese_units.id'), nullable=False)
+    # One grammar sheet belongs to one ChineseUnit
+    unit = relationship("ChineseUnit", back_populates="grammar_sheets")
+
+    def __repr__(self):
+        return f"<ChineseGrammarSheet(title='{self.title}', level='{self.level}, unit_id='{self.unit_id}')>"
+
 Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
-    unit = ChineseUnit(
-        title="Unite 15: A la maison",
-        description='',
-        objectives="A l'oral: Decrire une maison, un appartement\nUtiliser 吧 et les directionnels\nA l'ecrit: 17 caracteres",
-        level='A2'
-    )
+    with open('data/units.json', 'r', encoding='utf-8') as f:
+        unitsdata = json.load(f)
+    for idx in range(len(unitsdata)):
+        unit = ChineseUnit(
+            title=unitsdata[idx]['title'],
+            description=unitsdata[idx].get('description', ''),
+            objectives=unitsdata[idx].get('objectives', ''),
+            level=unitsdata[idx]['level']
+        )
+        session.add(unit)
 
-    words = []
+        words = []
 
-    for _, row in pd.read_csv('vocabulary_csv/unit15.csv').iterrows():
-        words.append(ChineseVocabulary(
-            word=row["word"],
-            pinyin=row["pinyin"],
-            translation=row["translation"],
-            type=row["type"],
-            level=unit.level,
-            unit=unit
-        ))
+        for _, row in pd.read_csv(f'data/vocabulary/unit{idx}.csv').iterrows():
+            words.append(ChineseVocabulary(
+                word=row["word"],
+                pinyin=row["pinyin"],
+                translation=row["translation"],
+                type=row["type"],
+                level=unit.level,
+                unit=unit
+            ))
 
-    session.add_all(words)
+        session.add_all(words)
+
+        if os.path.exists(f'data/grammar/unit{idx}.json'):
+            grammar_sheets = []
+            with open(f'data/grammar/unit{idx}.json', 'r', encoding='utf-8') as f:
+                grammar_data = json.load(f)
+            for element in grammar_data:
+                grammar_sheets.append(ChineseGrammarSheet(
+                    title=element["title"],
+                    content=element["content"],
+                    level=unit.level,
+                    unit=unit
+                ))
+
+            session.add_all(grammar_sheets)
     try:
         session.commit()
     except Exception as e:
